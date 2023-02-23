00e3b0e6d6daabdbdd166f56e6a1487d162b0af4
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2023-02-23 15:57:38 -0800

    Adding astro project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'ErrorCode0';
+export const SITE_DESCRIPTION = 'The official webstite of ErrorCode0';
